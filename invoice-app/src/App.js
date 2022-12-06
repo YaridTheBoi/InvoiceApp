23728c5ef2d82b0d4146f71c05aa02333fd9034e
casper <kacper.chrostowski@gmail.com>
2022-12-06 19:44:24 +0100

    started project created navbar and logo

---
+++ invoice-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar.js'
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ invoice-app/src/components/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './navbar.css'
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className='navbar'>
+                    
+
+
+                    <div className='logo'>
+                        <div className='oval'>
+                            <div className='triangle'></div>
+                        </div>
+                    </div>
+
+                </div>
+                
+
+            </div>
+        );
+            
+        
+    }
+}
+export default Navbar;
