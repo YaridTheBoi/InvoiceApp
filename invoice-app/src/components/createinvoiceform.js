97a1b9093fe94b51502166864bcb2fbfee86e428
casper <kacper.chrostowski@gmail.com>
2022-12-19 23:50:25 +0100

    some styling with form

--- invoice-app/src/components/createinvoiceform.js
+++ invoice-app/src/components/createinvoiceform.js
@@ -11,34 +11,55 @@ const CreateInvoiceForm =() =>{
                 <label for='from-street'>Street Address</label>
                 <input id='from-street' type={'text'}></input>
 
-                <label for='from-city'>City</label>
-                <input id='from-city' type={'text'}></input>
+                <div className="address-strip">
+                    <div>
+                        <label for='from-city'>City</label>
+                        <input id='from-city' type={'text'} ></input>
+                    </div>
 
-                <label for='from-postcode'>Post Code</label>
-                <input id='from-postcode' type={'text'}></input>
+                    <div>
+                        <label for='from-postcode'>Post Code</label>
+                        <input id='from-postcode' type={'text'} ></input>
+                    </div>
 
-                <label for='from-country'>Country</label>
-                <input id='from-country' type={'text'}></input>
+                    <div>
+                        <label for='from-country'>Country</label>
+                        <input id='from-country' type={'text'}></input>
+                    </div>
+
+
+                </div>
+
 
                 <div className="form-section-title">Bill To</div>
 
-                <label for='from-clientname'>Client's name</label>
-                <input id='from-clientname' type={'text'}></input>
+                <label for='to-clientname'>Client's name</label>
+                <input id='to-clientname' type={'text'}></input>
 
-                <label for='from-clientmail'>Client's Email</label>
-                <input id='from-clientmail' type={"email"}></input>
+                <label for='to-clientmail'>Client's Email</label>
+                <input id='to-clientmail' type={"email"}></input>
 
-                <label for='from-street'>Street Address</label>
-                <input id='from-street' type={'text'}></input>
+                <label for='to-street'>Street Address</label>
+                <input id='to-street' type={'text'}></input>
 
-                <label for='from-city'>City</label>
-                <input id='from-city' type={'text'}></input>
+                <div className="address-strip">
+                    <div>
+                        <label for='to-city'>City</label>
+                        <input id='to-city' type={'text'} ></input>
+                    </div>
 
-                <label for='from-postcode'>Post Code</label>
-                <input id='from-postcode' type={'text'}></input>
+                    <div>
+                        <label for='to-postcode'>Post Code</label>
+                        <input id='to-postcode' type={'text'} ></input>
+                    </div>
 
-                <label for='from-country'>Country</label>
-                <input id='from-country' type={'text'}></input>
+                    <div>
+                        <label for='to-country'>Country</label>
+                        <input id='to-country' type={'text'}></input>
+                    </div>
+
+
+                </div>
             </form>
 
         </div>
